10f664aa4a3e0632647aa680590194a385606d15
alce65 <alejandro.cerezo@evosoft.es>
2020-03-02 10:37:29 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bnp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular-Bnp';
+}
